d8ffbc538ec0a132df4952543ca3e2c08a6737df
Mehul Singh <mehulsingh2022@vitbhopal.ac.in>
2024-04-10 21:50:54 +0530

    Backend

---
+++ Backend/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddlewae } from "./middlewares/error.js"
+import userRouter from "./Routes/UserRouter.js";
+// import blogRouter from "./Routes/BlogRouter.js";
+
+
+
+
+const app=express();
+dotenv.config({path:"./config/config.env"});
+
+app.use(cors({
+    origin:[],
+    methods:["GET" , "PUT", "DELETE", "POST"],
+    credentials: true
+}))
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use("/api/v1", userRouter);
+dbConnection();
+app.use(errorMiddlewae);
+
+export default app;
---
+++ Backend/controllers/userController.js
@@ -0,0 +1,67 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js"
+import Errorhandler, { errorMiddlewae } from "../middlewares/error.js"
+import { User } from "../models/userSchema.js"
+import { sendToken } from "../utils/jwtToken.js"
+
+export const register = catchAsyncErrors (async (req,res,next)=>{
+    const {name,email,password,phone,role,education} = req.body;
+    if(!name || !email || !password || !phone || !role || !education){
+        return next(new Errorhandler("Please fill full details"))
+    }
+    let user=await User.findOne({email});
+    if(user){
+        return next(new Errorhandler("User already exists", 400));
+    }
+    user = await User.create({
+        name,
+        email,
+        password,
+        phone,
+        role,
+        education,
+    });
+
+    sendToken(user,200,"user registered succesfully",res);
+    // res.status(200).json({
+    //     success:true,
+    //     message:"User registered ! ",
+    // })
+})
+
+export const login = catchAsyncErrors(async(req,res,next)=>{
+    const{email,password,role}=req.body;
+    if(!email || !password || !role){
+        return next(new Errorhandler("please fill full form",400));
+
+    }
+    const user= await User.findOne({email}).select("+password");
+    if(!user){
+        return next(new Errorhandler("invalid email or password",400))
+    }
+    const isPasswordMatch= await user.comparePassword(password);
+    if(!isPasswordMatch){
+        return next(new Errorhandler("Invalid email / password",400))
+    };
+    if(user.role!==role){
+        return next(new Errorhandler("User with provided role (${role}) not found",400 ));
+    }
+    // res.status(200).json({
+    //     success:true,
+    //     message:"User logged in ! ",
+    // });
+    sendToken(user,200,"user logged in succesfully",res);
+    
+});
+
+export const logout = catchAsyncErrors((req, res, next) => {
+    res
+      .status(200)
+      .cookie("token", "", {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+      })
+      .json({
+        success: true,
+        message: "User logged out!",
+      });
+  });
