442992d5220c16e6b76082c4f94ce7a27495032c
Mehul Singh <mehulsingh2022@vitbhopal.ac.in>
2024-04-14 14:53:50 +0530

    full setup directions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import "./App.css"
+import {BrowserRouter as Router ,Route, Routes} from "react-router-dom"
+import Home from "./Components/pages/Home"
+import Register from './Components/pages/Register'
+import Login from "./Components/pages/Login"
+import Blogs from "./Components/pages/Blogs"
+import SingleBlog from "./Components/pages/SingleBlog"
+import About from "./Components/pages/About"
+import AllAuthors from "./Components/pages/AllAuthors"
+import Dashboard from "./Components/pages/Dashboard"
+import UpdateBlog from "./Components/pages/UpdateBlog"
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/> }/>
+          <Route path="/register" element={<Register/> }/>
+          <Route path="/login" element={<Login /> }/>
+          <Route path="/blogs" element={<Blogs/> }/>
+          <Route path="/blog/:id" element={<SingleBlog/> }/>
+          <Route path="/about" element={<About/> }/>
+          <Route path="/authors" element={<AllAuthors/> }/>
+          <Route path="/dashboard" element={<Dashboard/> }/>
+          <Route path="/bblog.update.:id" element={<UpdateBlog/> }/>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+/* eslint-disable no-unused-vars */
+import React, { createContext, useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+export const Context = createContext({
+  isAuthenticated: false,
+});
+
+const Appwrap = () => {
+  const [isAuthenticated, setisAuthenticated] = useState(false);
+  const [user, setuser] = useState({});
+  const [blogs, setBlogs] = useState({});
+  const [mode, setMode] = useState("dark");
+
+  return (
+    <Context.Provider
+      value={{
+        isAuthenticated,
+        setisAuthenticated,
+        user,
+        setuser,
+        blogs,
+        setBlogs,
+        mode,
+        setMode,
+      }}
+    >
+      <App />
+    </Context.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Appwrap />
+  </React.StrictMode>
+);
